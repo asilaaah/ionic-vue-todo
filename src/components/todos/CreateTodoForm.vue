ab7dca372d2b9d016ac5b828c110e20f72a33a56
asilaaah <asilah2110@gmail.com>
2022-06-16 15:47:14 +0800

    add camera feature in create form

--- src/components/todos/CreateTodoForm.vue
+++ src/components/todos/CreateTodoForm.vue
@@ -5,37 +5,58 @@
             <ion-input v-model="title" type="text" required></ion-input>
         </ion-item>
         <ion-item>
-            <ion-label position="floating">Image URL</ion-label>
-            <ion-input v-model="imageUrl" type="url" required></ion-input>
+            <ion-thumbnail>
+                <ion-img v-if="takenImageUrl" :src="takenImageUrl" />
+                <ion-img v-else src="https://acadianakarate.com/wp-content/uploads/2017/04/default-image.jpg"/>
+            </ion-thumbnail>
+            <ion-button type="button" fill="clear" @click="takePhoto">
+                <ion-icon slot="start" :icon="camera"></ion-icon>
+                Take Photo
+            </ion-button>
         </ion-item>
             <ion-button class="ion-margin-top" expand="block" type="submit">Create</ion-button>
     </form>
 </template>
 
 <script>
-import { IonItem, IonLabel, IonInput, IonButton } from '@ionic/vue';
+import { IonItem, IonLabel, IonInput, IonButton, IonThumbnail, IonImg, IonIcon } from '@ionic/vue';
+import { camera } from 'ionicons/icons';
+import { Camera, CameraResultType } from '@capacitor/camera';
+
 export default{
     emits: ['save-todo'],
     data(){
         return{
             title: '',
-            imageUrl: ''
+            takenImageUrl: null,
+            camera
         }
     },
     components: {
         IonItem, 
         IonLabel,
         IonInput,
-        IonButton
+        IonButton,
+        IonThumbnail,
+        IonImg,
+        IonIcon
     },
 
     methods: {
         submitForm(){
             const todoData = {
                 title: this.title,
-                image: this.imageUrl
+                image: this.takenImageUrl
             }
             this.$emit('save-todo', todoData)
+        },
+
+        async takePhoto(){
+            const image = await Camera.getPhoto({
+                quality: 90,
+                resultType: CameraResultType.Uri
+            });
+            this.takenImageUrl = image.webPath;
         }
     }
 }
